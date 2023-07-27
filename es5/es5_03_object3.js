5298158e9e72e2ca45e96f1023c1a438ec933339
leekm20210408 <lkm1401@gmail.com>
2023-07-27 10:56:36 +0900

    vs es5 commit~

--- es5/es5_03_object3.js
+++ es5/es5_03_object3.js
@@ -1,7 +1,7 @@
 const car = {
     wheels : 4,
     color : "red",
-    "goes fast" : true
+    "goes fast" : true   //속성이 문장인 경우에는 따옴표로 묶기
 };
 
 //console.log(car.goes fast);  //에러 발생
--- test.js
+++
@@ -1,2 +0,0 @@
-var user_name = "hong";
-console.log(user_name);
