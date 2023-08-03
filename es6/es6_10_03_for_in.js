aa81dd2f92c4ad5406288d4aedf0b7988c62c70f
leekm20210408 <lkm1401@gmail.com>
2023-08-03 10:15:01 +0900

    mac commit~

---
+++ es6/es6_10_03_for_in.js
@@ -0,0 +1,16 @@
+/** for in */
+const car = {
+    maker : "BMW",
+    color : "red",
+    year : "2010",
+};
+
+for(const prop in car){
+    console.log(prop, car[prop]);
+}
+
+
+
+
+
+
---
+++ es6/es6_11_05_Arrays_findIndex().js
@@ -0,0 +1,8 @@
+/** Array.find() */
+const greeting = ["hello", "hi", "byebye", "goodbye", "hi"];
+
+let foundIndex = greeting.findIndex(e => e === "hi"); 
+console.log(foundIndex);  // 1
+
+
+
